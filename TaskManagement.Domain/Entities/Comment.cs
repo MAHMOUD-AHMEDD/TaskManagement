77fab9bf94221f571705ced024ee22fcc61d5702
MAHMOUD-AHMEDD <166876691+MAHMOUD-AHMEDD@users.noreply.github.com>
2026-04-10 00:21:49 +0200

    fix: null safety on all entities, nullable DueDate/Description, fix Task ambiguity in IUnitOfWork

--- TaskManagement.Domain/Entities/Comment.cs
+++ TaskManagement.Domain/Entities/Comment.cs
@@ -1,14 +1,13 @@
-﻿using TaskManagement.Domain.Common;
+using TaskManagement.Domain.Common;
 
 namespace TaskManagement.Domain.Entities
 {
     public class Comment : BaseEntity
     {
-        public string Content { get; set; }
-        public string UserId { get; set; }
+        public string Content { get; set; } = null!;
+        public string UserId { get; set; } = null!;
         public int TaskId { get; set; }
-        public User User { get; set; }
-        public Task Task { get; set; }
-
+        public User User { get; set; } = null!;
+        public Task Task { get; set; } = null!;
     }
 }
--- TaskManagement.Domain/Entities/Task.cs
+++ TaskManagement.Domain/Entities/Task.cs
@@ -1,18 +1,19 @@
-﻿using TaskManagement.Domain.Common;
+using TaskManagement.Domain.Common;
 using TaskManagement.Domain.Enums;
+
 namespace TaskManagement.Domain.Entities
 {
     public class Task : BaseEntity
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
         public Enums.TaskStatus Status { get; set; }
         public TaskPriority Priority { get; set; }
-        public DateTime DueDate { get; set; }
+        public DateTime? DueDate { get; set; }
         public int ProjectId { get; set; }
-        public Project Project { get; set; }
-        public ICollection<TaskAssignment> TaskAssignments { get; set; }
-        public ICollection<Comment> Comments { get; set; }
-        public ICollection<TaskLabel> TaskLabels { get; set; }
+        public Project Project { get; set; } = null!;
+        public ICollection<TaskAssignment> TaskAssignments { get; set; } = new List<TaskAssignment>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public ICollection<TaskLabel> TaskLabels { get; set; } = new List<TaskLabel>();
     }
 }
